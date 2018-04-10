70f85d2d466caa18237e0754b56964f2ae3bec49
piotrkorpas <piotrkorpas@op.pl>
2018-04-10 20:44:47 +0200

    pierwsza wersja

---
+++ students/CMakeFiles/RootProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RootProject.
+# This may be replaced when dependencies are built.
---
+++ students/server/sources/CMakeFiles/RootProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RootProject.dir/main.cpp.o"
+  "RootProject.pdb"
+  "RootProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RootProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
