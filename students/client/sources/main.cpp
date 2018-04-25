0ac2b49f7b01689ab6b2757ae899b3e9bd27cb6b
PiotrekKiczmach <kiczmach.p@gmail.com>
2018-04-25 10:01:14 +0200

    Registration & Login form with database

--- students/client/sources/main.cpp
+++ students/client/sources/main.cpp
@@ -23,7 +23,6 @@ struct Register {
     char username[128];
     char passwd[128];
     char repasswd[128];
-    char email[128];
 };
 
 int main(int argc, char const *argv[])
@@ -31,8 +30,6 @@ int main(int argc, char const *argv[])
     struct sockaddr_in address;
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[1024] = {0};
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
@@ -61,6 +58,7 @@ int main(int argc, char const *argv[])
     int choise;
     unsigned int msgSize;
     Header* msg;
+    char buffer[1024];
 
     std::cout<<"Rejestracja - 2\nLogowanie - 1\n";
     std::cin>>choise;
@@ -70,16 +68,27 @@ int main(int argc, char const *argv[])
             {
                 msgSize = sizeof(Header) * sizeof(Login) - sizeof(char);
         
-                Login loginData = {"Piotrek", "Haslo"};
+                Login* loginData = new Login; //= {"Piotrek", "Haslo"};
 
                 msg = (Header*)malloc(msgSize);
 
                 msg->msgID = 1;
                 msg->contentSize = sizeof(Login);
 
-                memcpy(msg->payload, &loginData, sizeof(Login));
+                printf("Zaloguj się!\nPodaj swoją nazwę użytkownika (Login):\n");
+                std::cin>> loginData->username;
+                printf("\nPodaj swoje hasło:\n");
+                std::cin>> loginData->passwd;
+
+                memcpy(msg->payload, loginData, sizeof(Login));
 
                 send(sock, (char*)msg, msgSize, 0);
+
+                if(recv(sock, buffer, 1024, 0) > 0) {
+                    Header* receiveMsg = (Header*)buffer;
+                    
+                    std::cout<< receiveMsg->payload <<std::endl;
+                }
 
                 break;
             }
@@ -87,16 +96,29 @@ int main(int argc, char const *argv[])
             {
                 msgSize = sizeof(Header) * sizeof(Register) - sizeof(char);
         
-                Register registerData = {"user", "passwd", "passwd", "mail@mail.xx"};
+                Register* registerData  = new Register;//= {"user", "passwd", "passwd"}; //
 
                 msg = (Header*)malloc(msgSize);
 
                 msg->msgID = 2;
                 msg->contentSize = sizeof(Register);
 
-                memcpy(msg->payload, &registerData, sizeof(Register));
+                printf("Zarejestruj się już teraz!\nPodaj swoją nazwę użytkownika (Login):\n");
+                std::cin>>registerData->username;
+                printf("Podaj swoje hasło:\n");
+                std::cin>>registerData->passwd;
+                printf("Podaj ponownie hasło:\n");
+                std::cin>>registerData->repasswd;
 
-                send(sock, (char*)msg, msgSize, 0);
+                if(strcmp(registerData->passwd, registerData->repasswd) != 0) {
+                    printf("\nHasła roznią się\n");
+                    break;
+                }
+                else {
+                    memcpy(msg->payload, &registerData, sizeof(Register));
+
+                    send(sock, (char*)msg, msgSize, 0);
+                }
 
                 break;
             }
--- students/server/sources/main.cpp
+++ students/server/sources/main.cpp
@@ -7,6 +7,10 @@
 #include <arpa/inet.h>
 #include <thread>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <string>
 #define PORT 8080
 
 struct Header {
@@ -24,44 +28,166 @@ struct Register {
     char username[128];
     char passwd[128];
     char repasswd[128];
-    char email[128];
 };
+
+const char* database = "database.txt";
+std::vector<Login> loginData;
+
+void inputData(std::fstream &file, Register* data, std::vector<Login> vec) {
+    for(unsigned int i = 0; i < vec.size(); i++) {
+        file<<data->username<<" ";
+        file<<data->passwd<<"\n";
+    }
+
+}
+
+bool fileExist(const char* name) {
+    std::fstream file(name);
+
+    if(!file.good()) return false;
+
+    file.close();
+
+    return true;
+}
+
+void saveData(const char* name, Register* data, const std::vector<Login> vec) {
+    std::fstream file(name, std::ios::app | std::ios::ate);
+
+    if(!file.good()) {
+        printf("Nie mozna otworzyc bazy danych!\n");
+        exit(EXIT_FAILURE);
+    }
+    inputData(file,data, vec);
+    file.close();
+}
+
+std::vector<Login> readData(const char* name) {
+    
+    std::vector<Login> data;
+
+    std::fstream file(name, std::ios::in);
+
+    if(!file.good()) {
+        printf("Błąd podczas próby dostępu do bazy danych!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    Login tempData;
+
+    while(!file.eof()) {
+        
+        file>>tempData.username;
+        file>>tempData.passwd;
+    
+        data.push_back(tempData);
+    }
+    return data;
+}
 
 void clientHandler(int new_socket) {
     printf("Polaczono!\n");
 
     char buffer[1024];
+
     
     recv(new_socket, buffer, 1024, 0);
 
     Header* receiveMsg = (Header*)buffer;
+    Header* msg;
+    unsigned int msgSize;
+    //std::cout<< receiveMsg->msgID <<std::endl;
+    //std::cout<< receiveMsg->contentSize <<std::endl;
 
-    std::cout<< receiveMsg->msgID <<std::endl;
-    std::cout<< receiveMsg->contentSize <<std::endl;
+    std::vector<Login> user = readData(database);
+    std::cout<< user[0].username<< " " << user[0].passwd;
 
     if(receiveMsg->msgID == 1) {
-        printf("Ktos zalogował sie\nDane logowania:\n");
         
+        char error[128] = "Podano nieprawidłowe dane!\n";
+        char success[128] = "Zalogowano pomyślnie!\n";
+        msgSize = sizeof(Header) * sizeof(Login) - sizeof(char);
+
         Login* receivePayload = (Login*)(receiveMsg->payload);
 
-        std::cout<< receivePayload->username <<std::endl;
-        std::cout<< receivePayload->passwd <<std::endl;
+        for(unsigned int i = 0;i < user.size(); i++) {
+            Login userInfo = user.at(i);
+
+            if((receivePayload->username != userInfo.username) && (receivePayload->passwd != userInfo.passwd)) {
+
+                msg = (Header*)malloc(msgSize);
+
+                memcpy(msg->payload, &error, sizeof(error));
+                
+                send(new_socket, (char*)msg, msgSize, 0);
+                break;
+            }
+            else {
+
+                msg = (Header*)malloc(msgSize);
+
+                memcpy(msg->payload, &success, sizeof(success));
+                
+                send(new_socket, (char*)msg, msgSize, 0);
+                break;
+
+                printf("Ktos zalogował sie\nDane logowania:\n");
+        
+                std::cout<< receivePayload->username <<std::endl;
+                std::cout<< receivePayload->passwd <<std::endl;
+            }
+        }
+
+        
     }
     else if(receiveMsg->msgID == 2) {
-        printf("Ktos zarejestrowal sie\nDane rejestracji:\n");
+        
 
         Register* receivePayload = (Register*)(receiveMsg->payload);
 
-        std::cout<< receivePayload->username <<std::endl;
-        std::cout<< receivePayload->passwd <<std::endl;
-        std::cout<< receivePayload->email <<std::endl;
+        char error[128] = "Taki użytkownik już istnieje!\n";
+        char success[128] = "Zarejestrowano pomyślnie!\n";
+
+        for(unsigned int i = 0; i <user.size(); i++) {
+            Login userInfo = user.at(i);
+
+            if(receivePayload->username != userInfo.username) {
+
+                Login loginInfo;
+
+                msg = (Header*)malloc(msgSize);
+
+                printf("Ktos zarejestrowal sie\nDane rejestracji:\n");
+                std::cout<< receivePayload->username <<std::endl;
+                std::cout<< receivePayload->passwd <<std::endl;
+
+                memcpy(&loginInfo, msg->payload, sizeof(Login));
+
+                loginData.push_back(loginInfo);
+
+                memcpy(msg->payload, &success, sizeof(success));
+                
+                send(new_socket, (char*)msg, msgSize, 0);
+
+                break;
+            }
+            else {
+
+                msg = (Header*)malloc(msgSize);
+
+                memcpy(msg->payload, &error, sizeof(error));
+                
+                send(new_socket, (char*)msg, msgSize, 0);
+                break;
+            }
+        }
     }
 
 }
 
 int main(int argc, char const *argv[])
 {
-    int server_fd, new_socket, valread;
+    int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
@@ -94,6 +220,10 @@ int main(int argc, char const *argv[])
     if (listen(server_fd, 10) < 0)
     {
         perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if(!fileExist(database)) {
+        printf("Brak bazy danych!!!\n");
         exit(EXIT_FAILURE);
     }
     printf("Serwer odpalony\n");
